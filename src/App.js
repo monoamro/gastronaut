46da6ef54d10399c8759125e995986cd3fa3afbd
Montaser Amro <mono.amro@gmail.com>
2020-09-30 01:33:56 +0200

    Installs axios, and gets data from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import RestaurantInfo from './components/RestaurantInfo';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [restaurant, setRestaurant] = useState(null);
+  const [language, setLanguage] = useState("de");
+  const neoHeidelbergApiCall = "https://api.gastronaut.ai/codeTest/neo-heidelberg";
+  const schillingRoofBarApiCall = "https://api.gastronaut.ai/codeTest/schillingroofbar";
+
+  useEffect(() => {
+    axios.get(neoHeidelbergApiCall)
+          .then((result) => {
+            const {data} = result;
+            const {logo, link, image, events, regularHours, colorPalette, products, name, homeAddress} = data;
+            setRestaurant({logo, link, image, events, regularHours, colorPalette, products, name, homeAddress}); 
+          }, [])
+          .catch((e) => {console.log(e)})
+  }, []);
+  
+  console.log(restaurant);
+
   return (
     <Grid container>
-      <Grid container item>
-        <Header></Header>
-      </Grid>
+        <Header 
+        name={restaurant.name} 
+        logo={restaurant.logo} 
+        link={restaurant.link} 
+        language={language} 
+        setSelectedLanguage={(selLang) => setLanguage(selLang)}
+        />
       <Grid container item>
         <RestaurantInfo></RestaurantInfo>
       </Grid>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import LanguageToggle from "../LanguageToggle";
 import './index.css';
 
-const Header = () => {
+const Header = (name, logo, link, lang, setSelectedLang) => {
     return (
-        <h1>Header</h1>
+        <Grid container item xs={12} justify={"space-between"} alignItems={"center"} className="header">
+            <Grid item>
+                <a href={link} target="_blank">
+                    <img src={logo} alt={name}/>
+                </a>
+            </Grid>
+            <Grid item>
+                <LanguageToggle language={lang} setlanguage={setSelectedLang}/>
+            </Grid>
+        </Grid>
     )
 }
 
