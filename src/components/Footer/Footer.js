838e14924acf3f7eed1f77b9366bb093c9b80387
Montaser Amro <mono.amro@gmail.com>
2020-09-30 12:32:17 +0200

    Adds footer and links logo to website

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import './index.css';
 
 const Footer = () => {
     return (
-        <h1>Footer</h1>
+        <Grid container item xs={12} justify={"center"}>
+            <a target="_blank" href="https://www.gastronaut.ai">
+                <img src="../../images/poweredByGastronaut.png" alt="Powered by Gastronaut" className="gastro-logo"/>
+            </a>
+        </Grid>
     )
 }
 
